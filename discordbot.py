9a367c25682c7b49786509a6f840648e7b6c8365
DuskBotAdmin <71572567+DuskBotAdmin@users.noreply.github.com>
2020-09-22 19:15:57 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,10 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
+   
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Ping == {0}'.format(bot.latency,1))   
 
 bot.run(token)
