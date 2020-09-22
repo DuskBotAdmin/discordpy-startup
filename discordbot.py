4438764a4f83f617344dea2294c57902833b022e
DuskBotAdmin <71572567+DuskBotAdmin@users.noreply.github.com>
2020-09-22 19:27:05 +0900

    pingを追加

--- discordbot.py
+++ discordbot.py
@@ -12,14 +12,9 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
    
 @bot.command()
-async def .ping(ctx):
+async def ping(ctx):
     await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))   
 
 bot.run(token)
