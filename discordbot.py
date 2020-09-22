c79ed390f39a373146962265bcfd074e8597b74d
DuskBotAdmin <71572567+DuskBotAdmin@users.noreply.github.com>
2020-09-22 19:19:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -19,7 +19,7 @@ async def ping(ctx):
     
    
 @bot.command()
-async def ping2(ctx):
-    await ctx.send('Ping == {0}'.format(bot.latency))   
+async def ping(ctx):
+    await ctx.send('Pong! {0}'.format(round(bot.latency, 1)))   
 
 bot.run(token)
